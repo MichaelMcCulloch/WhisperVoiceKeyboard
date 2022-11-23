6a3652b1d697ec277fc08f0325d5dc28218b3c4f
Michael McCulloch <michael.james.mcculloch@fastmail.com>
2022-11-23 16:33:58 -0700

    Upgrade to ndk 25 (or 33?) by building tflitec dynamic lib under version 21, then upgrading and linking. Hopefully nothing explodes!

--- app/src/main/rust/.vscode/build_for_android.sh
+++ app/src/main/rust/.vscode/build_for_android.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 source .vscode/cargoNdkEnv.sh
-cargo ndk --bindgen --target=aarch64-linux-android   --target=x86_64-linux-android   build
+# cargo ndk --bindgen --target=aarch64-linux-android   --target=x86_64-linux-android   clean
+# cargo ndk --bindgen --target=aarch64-linux-android   --target=x86_64-linux-android   clean --release
+cargo ndk -p 33 --bindgen --target=aarch64-linux-android   --target=x86_64-linux-android   build
+# cargo ndk --bindgen --target=aarch64-linux-android   --target=x86_64-linux-android   build --release
