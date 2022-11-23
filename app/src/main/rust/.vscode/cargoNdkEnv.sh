eda0ad483fc511f8be7457d417e2b824adbd99c0
Michael McCulloch <michael.james.mcculloch@fastmail.com>
2022-11-23 16:33:58 -0700

    Upgrade to ndk 25 (or 33?) by building tflitec dynamic lib under version 21, then upgrading and linking. Hopefully nothing explodes!

--- app/src/main/rust/.vscode/cargoNdkEnv.sh
+++ app/src/main/rust/.vscode/cargoNdkEnv.sh
@@ -2,11 +2,13 @@
 
 
 export ANDROID_SDK_HOME="/home/michael/Android/Sdk"
-export ANDROID_NDK_API_LEVEL=21
-export ANDROID_SDK_VERSION=21
-export ANDROID_API_LEVEL=21
+export ANDROID_NDK_API_LEVEL=33
+export ANDROID_SDK_VERSION=33
+export ANDROID_API_LEVEL=33
 export ANDROID_BUILD_TOOLS_VERSION=33.0.0
-export ANDROID_NDK_HOME="$ANDROID_SDK_HOME/ndk/21.4.7075529"
-export ANDROID_HOME="$ANDROID_SDK_HOME/build-tools/21.1.2"
-export BINDGEN_EXTRA_CLANG_ARGS_x86_64_LINUX_ANDROID="-I/home/michael/Android/Sdk/ndk/21.4.7075529/sources/cxx-stl/llvm-libc++/include/ -I/home/michael/Android/Sdk/ndk/21.4.7075529/sysroot/usr/include/ -I/home/michael/Android/Sdk/ndk/21.4.7075529/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include/x86_64_linux_android/",
-export BINDGEN_EXTRA_CLANG_ARGS_AARCH64_LINUX_ANDROID="-I/home/michael/Android/Sdk/ndk/21.4.7075529/sources/cxx-stl/llvm-libc++/include/ -I/home/michael/Android/Sdk/ndk/21.4.7075529/sysroot/usr/include/ -I/home/michael/Android/Sdk/ndk/21.4.7075529/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include/aarch64_linux_android/"
+export ANDROID_NDK_HOME="$ANDROID_SDK_HOME/ndk/25.1.8937393"
+export ANDROID_HOME="$ANDROID_SDK_HOME/build-tools/33.0.0"
+export BINDGEN_EXTRA_CLANG_ARGS_x86_64_LINUX_ANDROID="-I/home/michael/Android/Sdk/ndk/25.1.8937393/sources/cxx-stl/llvm-libc++/include/ -I/home/michael/Android/Sdk/ndk/25.1.8937393/sysroot/usr/include/ -I/home/michael/Android/Sdk/ndk/25.1.8937393/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include/x86_64_linux_android/",
+export BINDGEN_EXTRA_CLANG_ARGS_AARCH64_LINUX_ANDROID="-I/home/michael/Android/Sdk/ndk/25.1.8937393/sources/cxx-stl/llvm-libc++/include/ -I/home/michael/Android/Sdk/ndk/25.1.8937393/sysroot/usr/include/ -I/home/michael/Android/Sdk/ndk/25.1.8937393/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include/aarch64_linux_android/"
+export TFLITEC_PREBUILT_PATH_x86_64_LINUX_ANDROID="/home/michael/AndroidStudioProjects/WhisperVoiceKeyboard/app/src/main/jniLibs/x86_64/libtensorflowlite_c.so"
+export TFLITEC_PREBUILT_PATH_AARCH64_LINUX_ANDROID="/home/michael/AndroidStudioProjects/WhisperVoiceKeyboard/app/src/main/jniLibs/arm64-v8a/libtensorflowlite_c.so"
