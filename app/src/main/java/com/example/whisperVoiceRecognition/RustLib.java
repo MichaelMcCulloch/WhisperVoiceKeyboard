5190985fc059d92c4bbaae9c92261abf2c1a516f
Michael McCulloch <michael.james.mcculloch@fastmail.com>
2022-12-05 06:48:24 -0700

    use a version of ac-ffmpeg with per-target build env

--- app/src/main/java/com/example/whisperVoiceRecognition/RustLib.java
+++ app/src/main/java/com/example/whisperVoiceRecognition/RustLib.java
@@ -5,7 +5,15 @@ import java.nio.ByteBuffer;
 public class RustLib {
 
     static {
+//        System.loadLibrary("avutil");
+//        System.loadLibrary("avcodec");
+//        System.loadLibrary("avformat");
+//        System.loadLibrary("swresample");
+//        System.loadLibrary("swscale");
+//        System.loadLibrary("avfilter");
+//        System.loadLibrary("avdevice");
         System.loadLibrary("rust");
+
         RustLib.initLogger();
     }
 
--- app/src/main/rust/.vscode/cargoNdkEnv.sh
+++ app/src/main/rust/.vscode/cargoNdkEnv.sh
@@ -2,8 +2,7 @@
 
 export PROJECT_ROOT="$HOME/AndroidStudioProjects/WhisperVoiceKeyboard"
 export JNI_LIB_DIR="$PROJECT_ROOT/app/src/main/jniLibs"
-export TFLITEC_PREBUILT_PATH_X86_64_LINUX_ANDROID="$JNI_LIB_DIR/x86_64/libtensorflowlite_c.so"
-export TFLITEC_PREBUILT_PATH_AARCH64_LINUX_ANDROID="$JNI_LIB_DIR/arm64-v8a/libtensorflowlite_c.so"
+export JNI_INCLUDE_DIR="$PROJECT_ROOT/app/src/main/jniInclude"
 export ANDROID_NDK_API_LEVEL=30
 export ANDROID_SDK_VERSION=30
 export ANDROID_API_LEVEL=30
@@ -12,8 +11,7 @@ export ANDROID_NDK_VERSION="25.1.8937393"
 export ANDROID_SDK_HOME="/home/michael/Android/Sdk"
 export ANDROID_HOME="$ANDROID_SDK_HOME/build-tools/$ANDROID_BUILD_TOOLS_VERSION"
 export ANDROID_NDK_HOME="$ANDROID_SDK_HOME/ndk/$ANDROID_NDK_VERSION"
-export BINDGEN_EXTRA_CLANG_ARGS_X86_64_LINUX_ANDROID="-I$ANDROID_NDK_HOME/sources/cxx-stl/llvm-libc++/include/ -I$ANDROID_NDK_HOME/sysroot/usr/include/ -I$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include/x86_64_linux_android/",
-export BINDGEN_EXTRA_CLANG_ARGS_AARCH64_LINUX_ANDROID="-I$ANDROID_NDK_HOME/sources/cxx-stl/llvm-libc++/include/ -I$ANDROID_NDK_HOME/sysroot/usr/include/ -I$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include/aarch64_linux_android/"
-
-
-export PKG_CONFIG_SYSROOT_DIR="$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/sysroot/"
+export FFMPEG_INCLUDE_DIR_AARCH64_LINUX_ANDROID="$JNI_INCLUDE_DIR/arm64-v8a/"
+export FFMPEG_LIB_DIR_AARCH64_LINUX_ANDROID="$JNI_LIB_DIR/arm64-v8a/"
+export FFMPEG_INCLUDE_DIR_X86_64_LINUX_ANDROID="$JNI_INCLUDE_DIR/x86_64/"
+export FFMPEG_LIB_DIR_X86_64_LINUX_ANDROID="$JNI_LIB_DIR/x86_64/"
