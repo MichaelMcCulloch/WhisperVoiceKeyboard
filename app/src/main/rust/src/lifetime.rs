e1d3b22593be55c85849d11b0603d730a906a429
Michael McCulloch <michael.james.mcculloch@fastmail.com>
2022-12-17 08:47:23 -0700

    use a matrix mult instead

--- app/src/main/rust/src/lifetime.rs
+++ app/src/main/rust/src/lifetime.rs
@@ -1,4 +1,4 @@
-use crate::{extract, statics::WHISPER_FILTERS};
+use crate::{consts::*, extract, statics::WHISPER_FILTERS};
 use ndk::asset::Asset;
 
 pub(crate) fn init(mut buffer: Asset) {
@@ -7,7 +7,14 @@ pub(crate) fn init(mut buffer: Asset) {
     match unsafe { WHISPER_FILTERS.take() } {
         None => {
             let filter = extract::extract_filters_and_vocab(&mut buffer).unwrap();
-            unsafe { WHISPER_FILTERS.replace(filter) };
+            let mut f = vec![vec![0.0; N_FFT]; N_MEL_BINS];
+            for i in 0..N_MEL_BINS {
+                f[i].copy_from_slice(&filter[i * N_FFT..(i + 1) * N_FFT])
+            }
+
+            log::info!("READ FILTERS");
+
+            unsafe { WHISPER_FILTERS.replace(transpose(f)) };
         }
         _ => {
             log::info!("Library already inititalized");
@@ -19,4 +26,18 @@ pub(crate) fn uninit() {
     unsafe {
         WHISPER_FILTERS.take();
     }
+}
+
+fn transpose<T>(v: Vec<Vec<T>>) -> Vec<Vec<T>> {
+    assert!(!v.is_empty());
+    let len = v[0].len();
+    let mut iters: Vec<_> = v.into_iter().map(|n| n.into_iter()).collect();
+    (0..len)
+        .map(|_| {
+            iters
+                .iter_mut()
+                .map(|n| n.next().unwrap())
+                .collect::<Vec<T>>()
+        })
+        .collect()
 }
