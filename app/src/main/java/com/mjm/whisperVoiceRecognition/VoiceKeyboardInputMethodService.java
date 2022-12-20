85e91e81f2e4ee9fb39267761d472133dac93567
Michael McCulloch <michael.james.mcculloch@fastmail.com>
2022-12-19 17:27:56 -0700

    A real delete key

--- app/src/main/java/com/mjm/whisperVoiceRecognition/VoiceKeyboardInputMethodService.java
+++ app/src/main/java/com/mjm/whisperVoiceRecognition/VoiceKeyboardInputMethodService.java
@@ -4,19 +4,16 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
-import android.graphics.Bitmap;
-import android.graphics.Color;
 import android.inputmethodservice.InputMethodService;
 import android.media.AudioDeviceInfo;
 import android.media.AudioManager;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
-import android.util.Pair;
+import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ToggleButton;
 
 import androidx.annotation.NonNull;
@@ -26,7 +23,6 @@ import com.example.WhisperVoiceKeyboard.R;
 import org.tensorflow.lite.Interpreter;
 
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
@@ -38,15 +34,20 @@ import java.util.OptionalInt;
 
 public class VoiceKeyboardInputMethodService extends InputMethodService {
 
-    private static Vocab _vocab;
     private static Interpreter _whisperInterpreter;
+    private static Dictionary _dictionary;
 
+    private static final boolean LOG_AND_DRAW = false;
 
     @Override
     public void onCreate() {
         super.onCreate();
         try {
-            _vocab = ExtractVocab.extractVocab(getAssets().open("filters_vocab_gen.bin"));
+            Vocab vocab = ExtractVocab.extractVocab(getAssets().open("filters_vocab_gen.bin"));
+            HashMap<String, String> phraseMappings = new HashMap<>();
+
+
+            _dictionary = new Dictionary(vocab, phraseMappings);
             MappedByteBuffer model = loadWhisperModel(getAssets());
             Log.i("TFLITE", "onCreateInputView: " + "Created tflitemodel");
 
@@ -74,25 +75,22 @@ public class VoiceKeyboardInputMethodService extends InputMethodService {
     @SuppressLint("ClickableViewAccessibility")
     @Override
     public View onCreateInputView() {
-        View inputView =
+        @SuppressLint("InflateParams") View inputView =
                 getLayoutInflater().inflate(R.layout.keyboard, null);
         ToggleButton recordButton = inputView.findViewById(R.id.buttonRecord);
         Button cancelButton = inputView.findViewById(R.id.buttonCancel);
-        ImageButton deleteButton = inputView.findViewById(R.id.buttonDelete);
-//        TextView signalTxt = inputView.findViewById(R.id.textSignalTime);
-//        TextView inferenceTxt = inputView.findViewById(R.id.textInferenceTime);
+        Button deleteButton = inputView.findViewById(R.id.buttonDelete);
 
-        cancelButton.setOnClickListener(v -> {
-            RustLib.abortRecording();
-            recordButton.setChecked(false);
-
+        deleteButton.setOnClickListener(view -> {
+            sendDelete();
         });
+
         deleteButton.setOnTouchListener(new View.OnTouchListener() {
             private Handler mHandler;
             final Runnable mAction = new Runnable() {
                 @Override
                 public void run() {
-                    getCurrentInputConnection().deleteSurroundingText(1, 0);
+                    sendDelete();
                     mHandler.postDelayed(this, 100);
                 }
             };
@@ -103,7 +101,7 @@ public class VoiceKeyboardInputMethodService extends InputMethodService {
                     case MotionEvent.ACTION_DOWN:
                         if (mHandler != null) return true;
                         mHandler = new Handler(Looper.getMainLooper());
-                        mHandler.postDelayed(mAction, 100);
+                        mHandler.postDelayed(mAction, 0);
                         return true;
                     case MotionEvent.ACTION_UP:
                     case MotionEvent.ACTION_CANCEL:
@@ -115,31 +113,38 @@ public class VoiceKeyboardInputMethodService extends InputMethodService {
                 return false;
             }
         });
+
+        cancelButton.setOnClickListener(v -> {
+            RustLib.abortRecording();
+            recordButton.setChecked(false);
+        });
+
         recordButton.setOnCheckedChangeListener((button, checked) -> {
             if (checked && getBottomMicrophone().isPresent()) {
                 RustLib.startRecording(getBottomMicrophone().get());
-
             } else {
-                Pair<Optional<float[]>, Long> byteBuffer = RustLib.endRec();
-
-                if (byteBuffer.first.isPresent()) {
-                    draw(byteBuffer.first.get());
-//                    signalTxt.setText(new String(byteBuffer.second / 1000_000 + " ms"));
-                    Pair<String, Long> transcribeAudio = transcribeAudio(byteBuffer.first.get());
-                    String transcribed = transcribeAudio.first.trim() + " ";
-
+                Optional<float[]> byteBuffer = RustLib.endRec();
+                if (byteBuffer.isPresent()) {
+                    String transcribeAudio = transcribeAudio(byteBuffer.get());
+                    String transcribed = transcribeAudio.trim() + " ";
                     getCurrentInputConnection().commitText(transcribed, transcribed.length());
-//                    inferenceTxt.setText(new String(transcribeAudio.second / 1000_000 + " ms"));
-
+                    if (LOG_AND_DRAW) {
+                        SpectrogramToFile.save(byteBuffer.get(), getFilesDir().getAbsolutePath());
+                    }
                 }
             }
         });
-
         return inputView;
     }
 
+    private void sendDelete() {
+        getCurrentInputConnection().sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DEL));
+        getCurrentInputConnection().sendKeyEvent(new KeyEvent(KeyEvent.ACTION_UP, KeyEvent.KEYCODE_DEL));
+    }
+
+
     @NonNull
-    private Pair<String, Long> transcribeAudio(float[] byteBuffer) {
+    private String transcribeAudio(float[] byteBuffer) {
         int[] inputShape = {1, 80, 3000};
 
         float[][][] reshapedFloats = reshapeInput(byteBuffer, inputShape);
@@ -150,31 +155,11 @@ public class VoiceKeyboardInputMethodService extends InputMethodService {
         Map<String, Object> outputs = new HashMap<>();
         outputs.put("sequences", output);
 
-        long alpha = System.nanoTime();
         _whisperInterpreter.runSignature(inputs, outputs, "serving_default");
-        long beta = System.nanoTime();
-        Log.i("RustLib", "runSignature: took" + (beta - alpha) + "nanos");
-
-
-        String transcribed = tokensToString(output);
-        return new Pair<>(transcribed, beta - alpha);
+        String whisperOutput = _dictionary.tokensToString(output);
+        return _dictionary.injectTokens(whisperOutput);
     }
 
-    @NonNull
-    private String tokensToString(int[][] output) {
-        StringBuilder sb = new StringBuilder();
-        for (int token : output[0]) {
-            if (token == _vocab.token_eot) {
-                break;
-            }
-            if (token != 50257 && token != 50362) {
-                String word = _vocab.id_to_token.get(token);
-                Log.i("tokenization", "token: " + token + " word " + word);
-                sb.append(word);
-            }
-        }
-        return sb.toString();
-    }
 
     @NonNull
     private float[][][] reshapeInput(float[] byteBuffer, int[] inputShape) {
@@ -189,33 +174,6 @@ public class VoiceKeyboardInputMethodService extends InputMethodService {
             }
         }
         return reshapedFloats;
-    }
-
-    private void draw(float[] floats) {
-
-
-        Bitmap bitmap = Bitmap.createBitmap(3000, 80, Bitmap.Config.ARGB_8888);
-
-        //Loop through the float array and save each value as a pixel in the bitmap
-        int x = 0;
-        int y = 0;
-        for (float f : floats) {
-            int color = (int) ((f + 1) / 2 * 255);
-            bitmap.setPixel(y, x, Color.argb(255, color, color, color));
-            x++;
-            if (x >= 80) {
-                x = 0;
-                y++;
-            }
-        }
-        //write the bitmap to file
-        try {
-            FileOutputStream out = new FileOutputStream(getFilesDir().getAbsolutePath() + "/spectrogram" + System.nanoTime() + ".png");
-            bitmap.compress(Bitmap.CompressFormat.PNG, 100, out);
-            out.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
 
